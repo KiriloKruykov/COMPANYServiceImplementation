17e9244571813ba748979f7eeac6f49f6126c122
KiriloKruykov <vipernonstop@ukr.net>
2020-06-23 22:52:22 +0300

    Initial commit

---
+++ src/com/company/Company.java
@@ -0,0 +1,69 @@
+/*
+ *
+ * Classname: Company
+ *
+ * @version 23.06.2020
+ * @author Kryukov Kirilo
+
+ * Module 4 task Final
+ *
+ *      COMPANY Service implementation - 15 points
+ *
+ *      1. Create a test set of companies in your main class.
+ *
+ *      2.  Take for this set  IT-companies:  Chief, Lawyer, Developer
+ *      (Design,  Front-end, Back-end, DevOps). See attch.
+ *
+ *      3. Take into account 4 different cases  for a company
+ *      ( on-top, on bottom, in the node,  out of chain)
+ *
+ *      4. Test all this cases in your main class.
+ *
+ *      5. Implement the interface in a class CompanyService.
+ *
+ *
+ *
+ */
+
+package com.company;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Company {
+    // parent for this company nullable, when there is no parent for this company
+    private Company parent;
+    private long employeesCount;
+//Contructor.
+    public Company() {
+    }
+
+    public Company(Company parent, long employeesCount) {
+        this.parent = parent;
+        this.employeesCount = employeesCount;
+    }
+//Getters , setters.
+    public Company getParent() {
+        return parent;
+    }
+
+    public void setParent(Company parent) {
+        this.parent = parent;
+    }
+
+    public long getEmployeesCount() {
+        return employeesCount;
+    }
+
+    public void setEmployeesCount(long employeesCount) {
+        this.employeesCount = employeesCount;
+    }
+//toString.
+    @Override
+    public String toString() {
+        return "Company{" +
+                "parent=" + parent +
+                ", employeesCount=" + employeesCount +
+                '}';
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,82 @@
+/*
+ *
+ * Classname: Main
+ *
+ * @version 23.06.2020
+ * @author Kryukov Kirilo
+
+ * Module 4 task Final
+ *
+ *      COMPANY Service implementation - 15 points
+ *
+ *      1. Create a test set of companies in your main class.
+ *
+ *      2.  Take for this set  IT-companies:  Chief, Lawyer, Developer
+ *      (Design,  Front-end, Back-end, DevOps). See attch.
+ *
+ *      3. Take into account 4 different cases  for a company
+ *      ( on-top, on bottom, in the node,  out of chain)
+ *
+ *      4. Test all this cases in your main class.
+ *
+ *      5. Implement the interface in a class CompanyService.
+ *
+ *
+ *
+ */
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        CompanyServiceImpl service = new CompanyServiceImpl();
+
+        List<Company> companies = new ArrayList<>();
+        Company chief = new Company(null,45);
+        Company lawyer = new Company(chief,23);
+        Company developer = new Company(chief,14);
+        Company backEnd = new Company(developer,20);
+        Company frontEnd = new Company(developer,14);
+        Company devOps = new Company(backEnd,16);
+        Company design = new Company(frontEnd,17);
+        Company accounting = new Company(null,6);
+
+        // test 1 child must return chief
+
+        System.out.println(service.getTopLevelParent(chief));
+        System.out.println(service.getTopLevelParent(developer));
+        System.out.println(service.getTopLevelParent(lawyer));
+        System.out.println(service.getTopLevelParent(backEnd));
+        System.out.println(service.getTopLevelParent(frontEnd));
+        System.out.println(service.getTopLevelParent(devOps));
+        System.out.println(service.getTopLevelParent(design));
+        System.out.println(service.getTopLevelParent(accounting));
+
+        Arrays.asList(chief,lawyer,developer,backEnd,frontEnd,devOps,design,accounting);
+
+
+        System.out.println("Parent company for company Chief is: \n"
+                + service.getTopLevelParent(chief));
+        System.out.println("Parent company for company Lawyer is: \n"
+                + service.getTopLevelParent(developer));
+        System.out.println("Parent company for company Developer is: \n"
+                + service.getTopLevelParent(lawyer));
+        System.out.println("Parent company for company Back-End is: \n"
+                + service.getTopLevelParent(backEnd));
+        System.out.println("Parent company for company Front-End is: \n"
+                + service.getTopLevelParent(frontEnd));
+        System.out.println("Parent company for company DevOps is: \n"
+                + service.getTopLevelParent(devOps));
+        System.out.println("Parent company for company Design is: \n"
+                + service.getTopLevelParent(design));
+        System.out.println("Parent company for company Accounting is: \n"
+                + service.getTopLevelParent(accounting));
+
+    }
+}
